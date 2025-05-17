6a0f7b176c5b34dfaeaf430e3e85508cf1f1bf15
Enzo Brum <darosabrumenzo@gmail.com>
2025-05-17 12:05:20 -0300

    fix latex

--- graphs/hungarian.cpp
+++ graphs/hungarian.cpp
@@ -1,41 +1,42 @@
 int A[251][251];
 
-// Matrix A is n*m - O(n²m). Returns the index chosen for each line (1 indexed).
+// Matrix A is n*m - O(n^2m). Returns the index chosen for each line (1
+// indexed).
 #warning A is 1 based
 vector<int> hungarian(int n, int m) {
-    vector<int> u(n+1), v(m+1), p(m+1), way(m+1);
-    for (int i=1; i<=n; ++i) {
-        p[0] = i;
-        int j0 = 0;
-        vector<int> minv(m+1, INF);
-        vector<bool> used(m+1, false);
-        do {
-            used[j0] = true;
-            int i0 = p[j0],  delta = INF,  j1;
-            for (int j=1; j<=m; ++j)
-                if (!used[j]) {
-                    int cur = A[i0][j]-u[i0]-v[j];
-                    if (cur < minv[j])
-                        minv[j] = cur,  way[j] = j0;
-                    if (minv[j] < delta)
-                        delta = minv[j],  j1 = j;
-                }
-            for (int j=0; j<=m; ++j)
-                if (used[j])
-                    u[p[j]] += delta,  v[j] -= delta;
-                else
-                    minv[j] -= delta;
-            j0 = j1;
-        } while (p[j0] != 0);
-        do {
-            int j1 = way[j0];
-            p[j0] = p[j1];
-            j0 = j1;
-        } while (j0);
-    }
-    vector<int> ans(n+1);
-    for (int j=1; j<=m; ++j)
-        ans[p[j]] = j;
-    return ans;
-    // int cost = -v[0];
+  vector<int> u(n + 1), v(m + 1), p(m + 1), way(m + 1);
+  for (int i = 1; i <= n; ++i) {
+    p[0] = i;
+    int j0 = 0;
+    vector<int> minv(m + 1, INF);
+    vector<bool> used(m + 1, false);
+    do {
+      used[j0] = true;
+      int i0 = p[j0], delta = INF, j1;
+      for (int j = 1; j <= m; ++j)
+        if (!used[j]) {
+          int cur = A[i0][j] - u[i0] - v[j];
+          if (cur < minv[j])
+            minv[j] = cur, way[j] = j0;
+          if (minv[j] < delta)
+            delta = minv[j], j1 = j;
+        }
+      for (int j = 0; j <= m; ++j)
+        if (used[j])
+          u[p[j]] += delta, v[j] -= delta;
+        else
+          minv[j] -= delta;
+      j0 = j1;
+    } while (p[j0] != 0);
+    do {
+      int j1 = way[j0];
+      p[j0] = p[j1];
+      j0 = j1;
+    } while (j0);
+  }
+  vector<int> ans(n + 1);
+  for (int j = 1; j <= m; ++j)
+    ans[p[j]] = j;
+  return ans;
+  // int cost = -v[0];
 }
